50dd0fee376757777a241b8da85f0cb22f09f81c
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-01-04 14:32:38 +0545

    security

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 
@@ -14,12 +22,20 @@ const jobRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max:100
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs',authUser, jobRouter);
+app.use('/api/v1/jobs', authUser, jobRouter);
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
